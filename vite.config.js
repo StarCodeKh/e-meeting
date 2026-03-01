e88c4c6e6555a336edaa1e3c6714c117ef3a9744
StarCode Kh <starcodekh@email.com>
2026-03-01 22:57:25 +0700

    update font

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,17 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.js', 'resources/css/app.css'],
-      refresh: true,
-    }),
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
+    plugins: [
+        laravel({
+        input: ['resources/js/app.js', 'resources/css/app.css'],
+        refresh: true,
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+        '@': path.resolve(__dirname, 'resources/js'),
+        '@fonts': path.resolve(__dirname, 'resources/js/assets/fonts'),
+        },
     },
-  },
 });
